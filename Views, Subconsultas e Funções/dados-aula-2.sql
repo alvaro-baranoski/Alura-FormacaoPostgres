01cbc342a97ad670f5ff1e72735678c6599ab6be
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-06-03 18:22:42 -0300

    anotações aula 5

--- Views, Subconsultas e Funções/dados-aula-2.sql
+++ Views, Subconsultas e Funções/dados-aula-2.sql
@@ -60,3 +60,18 @@ SELECT
 FROM aluno;
 
 SELECT TO_CHAR(NOW(), 'DD, MONTH, YYYY');
+
+-- Views
+CREATE VIEW vw_cursos_por_categoria AS
+	SELECT 
+		categoria.nome AS categoria,
+		COUNT(curso.id) AS numero_cursos
+	FROM categoria
+	JOIN curso ON curso.categoria_id = categoria.id
+	GROUP BY categoria
+	
+SELECT * FROM vw_cursos_por_categoria
+
+SELECT categoria.id, vw_cursos_por_categoria.* 
+	FROM vw_cursos_por_categoria
+	JOIN categoria ON categoria.nome = vw_cursos_por_categoria.categoria;
