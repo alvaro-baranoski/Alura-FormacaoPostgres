bcb076e208a5b4f1cb5323157655d920c6112250
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-06-06 02:17:43 -0300

    Anotações aula 5 - Triggers

--- Triggers, Transações, Erros e Cursores/anotacoes.sql
+++ Triggers, Transações, Erros e Cursores/anotacoes.sql
@@ -66,3 +66,28 @@ CREATE OR REPLACE FUNCTION instrutores_internos(id_instrutor INTEGER) RETURNS re
 		RETURN cursor_salario;
 	END;
 $$ LANGUAGE plpgsql;
+
+-- Blocos anonimos
+DO $$
+	DECLARE
+		cursor_salarios refcursor;
+		salario DECIMAL;
+		total_instrutores INTEGER DEFAULT 0;
+		instrutores_recebem_menos INTEGER DEFAULT 0;
+		percentual DECIMAL;
+	BEGIN
+		SELECT instrutores_internos(12) INTO cursor_salarios;
+		LOOP
+			FETCH cursor_salarios INTO salario;
+			EXIT WHEN NOT FOUND;
+			total_instrutores := total_instrutores + 1;
+			
+			IF 600::DECIMAL > salario THEN
+				instrutores_recebem_menos := instrutores_recebem_menos + 1;
+			END IF;
+		END LOOP;
+		percentual = instrutores_recebem_menos::DECIMAL / total_instrutores::DECIMAL * 100;
+		
+		RAISE NOTICE 'Percentual: % %%', percentual;
+	END;
+$$;
